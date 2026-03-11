59ec2a9d3c0ba1542711ce3231b052185035400a
Cauã Brito <143093695+CauaBt@users.noreply.github.com>
2026-03-11 20:51:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial', padding:'40px'}}>
+      <h1>Vite + React funcionando 🚀</h1>
+      <p>Seu projeto está rodando com Vite.</p>
+    </div>
+  )
+}
